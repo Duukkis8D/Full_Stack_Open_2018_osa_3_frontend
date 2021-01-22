1a97b566a66492895ee5e516438597e82b89627c
Tuukka Virtanen <duukkis@gmail.com>
2021-01-22 03:15:33 +0200

    CSS-tyylien kehittämistä

--- src/components/PersonsList.js
+++ src/components/PersonsList.js
@@ -7,20 +7,22 @@ const PersonsList = (props) => {
             props.persons.filter(person => person.name.toUpperCase().includes(props.search.toUpperCase()));
 
     return (
-        <table>
-            <thead><tr><th></th></tr></thead>
-            <tbody>
-                { persons.map(person => <tr key={person.name}>
-                                            <td key={person.name}>{person.name}</td>
-                                            <td key={person.phone}>{person.phone}</td>
-                                            <td>
-                                                <button onClick={props.removePerson(person.id, person.name)}>
-                                                    Poista
-                                                </button>
-                                            </td>
-                                        </tr>) }
-            </tbody>
-        </table>
+        <div id="personsListContainer">
+            <table>
+                <thead><tr><th></th></tr></thead>
+                <tbody>
+                    { persons.map(person => <tr key={person.name}>
+                                                <td key={person.name}>{person.name}</td>
+                                                <td key={person.phone}>{person.phone}</td>
+                                                <td>
+                                                    <button onClick={props.removePerson(person.id, person.name)}>
+                                                        Poista
+                                                    </button>
+                                                </td>
+                                            </tr>) }
+                </tbody>
+            </table>
+        </div>
     )
 };
 
